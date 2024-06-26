3077a0f4c452cafbd519110843b88dd93812027e
cleyton_orocha <cleytoodeoliveirarocha@gmail.com>
2024-06-26 20:46:34 -0300

    feat: main component and active side-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './template/side-bar/side-bar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    SideBarComponent
+    SideBarComponent,
+    MainComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'inovar-portiguar-adminFront';
+  
+  public activeSide : Boolean = true;
+ 
 }
